b5ef6c0f6522d788fa0adbee175563a0d131e0a4
levijcdf <levijcfigueiredowebdev@gmail.com>
2026-04-24 18:58:40 +0100

    Añadida funcionalidad de alerta al hacer clic en el botón y reestructurada la disposición del HTML y CSS

---
+++ app.js
@@ -0,0 +1,16 @@
+function showMessage() {
+    Swal.fire({
+        title: 'Esta aplicación es super compleja',
+        text: '(creeme)',
+        icon: 'info'
+    });
+}
+
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', function () {
+        showMessage();
+    });
+} else {
+    console.log("ERROR: No se ha encontrado boton");
+}
